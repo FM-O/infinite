4fa0000faa29d6891e19fa90a241afcf5bc4568c
Flo <florian.michel69250@gmail.com>
2016-11-03 19:59:02 +0100

    - Angular conf (initialized, TODO: divide into appropriate directories) - Project config OK - Header released (style and struct)

---
+++ API/socials.php
@@ -0,0 +1,5 @@
+<?php
+
+$json = file_get_contents('data/socials.json');
+
+echo $json;
--- js/controllers/core.js
+++ js/controllers/core.js
@@ -2,6 +2,38 @@
 
 var infinite = angular.module('infinite', ['ngRoute']);
 
-infinite.controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.text = "coucou";
+infinite.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.loadSocials = function () {
+        var request = $http({
+            method: "get",
+            url: "API/socials.php"
+        });
+
+        /* Check whether the HTTP Request is successful or not. */
+        request.success(function (data) {
+
+            console.log("data required successfully retrieved" + data);
+            $scope.socials = data;
+        });
+    }();
+
+    $scope.socials = "views/templates/socials.html";
+    $scope.txt = {
+        "txt1" : "coucou",
+        "txt2" : "hello"
+    };
+
 }]);
+
+infinite.directive('mainNav', function () {
+    return {
+        templateUrl: 'views/templates/mainNav.html',
+        restrict: 'A',
+        replace: true,
+        transclude: true,
+        link: function (scope, element, attrs) {
+            // DOM manipulation/events here!
+        }
+    }
+});
